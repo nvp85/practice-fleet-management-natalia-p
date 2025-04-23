54ac6198d5fb6415ffd25a183d98efc953b7ddf2
nvp85 <nvp@zhukov.al>
2025-04-23 15:17:15 -0500

    fix the getDetails method: the instructions require it return a string

--- app.js
+++ app.js
@@ -9,18 +9,19 @@ class Vehicle {
 
    drive(distance) {
         this.mileage += distance;
+        console.log(`The vehicle has been driven ${distance} miles.`);
    }
 
    getDetails() {
-        console.log(`Report:\nMake: ${this.make}\nModel: ${this.model}\nYear: ${this.year}\nMileage: ${this.mileage}`);
+        return `Report:\nMake: ${this.make}\nModel: ${this.model}\nYear: ${this.year}\nMileage: ${this.mileage}`;
    }
   }
 
 let car = new Vehicle("car", "toyota", "prius", 2020);
-car.getDetails();
+console.log(car.getDetails());
 car.drive(1000);
-car.getDetails(); // mileage is update
+console.log(car.getDetails()); // mileage is update
 let truck = new Vehicle("truck", "tpyota", "tacoma", 2022, 10000);
 let motorcycle = new Vehicle("motorcycle", "toyota", "prius", 2020, 4500);
-truck.getDetails();
-motorcycle.getDetails();
+console.log(truck.getDetails());
+console.log(motorcycle.getDetails());
